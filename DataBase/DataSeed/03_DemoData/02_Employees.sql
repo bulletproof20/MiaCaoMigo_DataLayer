eb2c7392550ea95bc95593ce79b4e8adf5bf6f04
Ivo Sa <ivo.carsa@gmail.com>
2026-05-30 10:16:33 +0100

    update hash pass

--- DataBase/DataSeed/03_DemoData/02_Employees.sql
+++ DataBase/DataSeed/03_DemoData/02_Employees.sql
@@ -13,12 +13,12 @@ insert into employee (
 )
 overriding system value
 values
-    (2, 2, '2026-05-01 09:00:00+01', 1, null, null, '+351911911912', '+351911911913', '2@miacaomigo.pt', '$2b$12$miacaomigo.narrative.demo.hash'),
-    (3, 3, '2026-05-04 08:00:00+01', 2, null, null, '+351922922923', null, '3@miacaomigo.pt', '$2b$12$miacaomigo.narrative.demo.hash'),
-    (4, 4, '2026-05-04 08:05:00+01', 2, null, null, '+351933933934', null, '4@miacaomigo.pt', '$2b$12$miacaomigo.narrative.demo.hash'),
-    (5, 5, '2026-05-04 08:10:00+01', 2, null, null, '+351944944945', '+351944944946', '5@miacaomigo.pt', '$2b$12$miacaomigo.narrative.demo.hash'),
-    (6, 6, '2026-05-05 08:00:00+01', 2, null, null, '+351955955956', null, '6@miacaomigo.pt', '$2b$12$miacaomigo.narrative.demo.hash'),
-    (7, 7, '2025-09-01 09:00:00+01', 1, '2026-03-15 17:00:00+01', 2, '+351912340299', null, '7@miacaomigo.pt', '$2b$12$miacaomigo.narrative.demo.hash');
+    (2, 2, '2026-05-01 09:00:00+01', 1, null, null, '+351911911912', '+351911911913', '2@miacaomigo.pt', '15e2b0d3c33891ebb0f1ef609ec419420c20e320ce94c65fbc8c3312448eb225'),
+    (3, 3, '2026-05-04 08:00:00+01', 2, null, null, '+351922922923', null, '3@miacaomigo.pt', '15e2b0d3c33891ebb0f1ef609ec419420c20e320ce94c65fbc8c3312448eb225'),
+    (4, 4, '2026-05-04 08:05:00+01', 2, null, null, '+351933933934', null, '4@miacaomigo.pt', '15e2b0d3c33891ebb0f1ef609ec419420c20e320ce94c65fbc8c3312448eb225'),
+    (5, 5, '2026-05-04 08:10:00+01', 2, null, null, '+351944944945', '+351944944946', '5@miacaomigo.pt', '15e2b0d3c33891ebb0f1ef609ec419420c20e320ce94c65fbc8c3312448eb225'),
+    (6, 6, '2026-05-05 08:00:00+01', 2, null, null, '+351955955956', null, '6@miacaomigo.pt', '15e2b0d3c33891ebb0f1ef609ec419420c20e320ce94c65fbc8c3312448eb225'),
+    (7, 7, '2025-09-01 09:00:00+01', 1, '2026-03-15 17:00:00+01', 2, '+351912340299', null, '7@miacaomigo.pt', '15e2b0d3c33891ebb0f1ef609ec419420c20e320ce94c65fbc8c3312448eb225');
 
 insert into assistant (id_emp, fun_ass) values
     (3, 'Animal care and shelter intake'),
--- DataBase/DataSeed/03_DemoData/08_Customers.sql
+++ DataBase/DataSeed/03_DemoData/08_Customers.sql
@@ -10,13 +10,13 @@ set timezone to 'Europe/Lisbon';
 insert into client (id_cli, id_usr, pas_cli, reg_dat_cli, ina_dat_cli)
 overriding system value
 values
-    (1, 8, '$2b$12$miacaomigo.narrative.demo.hash', '2026-05-06 10:30:00+01', null),
-    (2, 9, '$2b$12$miacaomigo.narrative.demo.hash', '2026-05-08 09:15:00+01', null),
-    (3, 10, '$2b$12$miacaomigo.narrative.demo.hash', '2026-05-06 11:00:00+01', null),
+    (1, 8, '15e2b0d3c33891ebb0f1ef609ec419420c20e320ce94c65fbc8c3312448eb225', '2026-05-06 10:30:00+01', null),
+    (2, 9, '15e2b0d3c33891ebb0f1ef609ec419420c20e320ce94c65fbc8c3312448eb225', '2026-05-08 09:15:00+01', null),
+    (3, 10, '15e2b0d3c33891ebb0f1ef609ec419420c20e320ce94c65fbc8c3312448eb225', '2026-05-06 11:00:00+01', null),
     -- Pedro inactive 2 Jun (story); clamped to wall-clock for ck_client_dates at seed load
-    (4, 11, '$2b$12$miacaomigo.narrative.demo.hash', '2026-05-07 14:00:00+01',
+    (4, 11, '15e2b0d3c33891ebb0f1ef609ec419420c20e320ce94c65fbc8c3312448eb225', '2026-05-07 14:00:00+01',
      least(timestamptz '2026-06-02 09:00:00+01', current_timestamp - interval '1 second')),
-    (5, 6, '$2b$12$miacaomigo.narrative.demo.hash', '2025-11-01 10:00:00+01', null);
+    (5, 6, '15e2b0d3c33891ebb0f1ef609ec419420c20e320ce94c65fbc8c3312448eb225', '2025-11-01 10:00:00+01', null);
 
 select setval(pg_get_serial_sequence('client', 'id_cli'),
     (select coalesce(max(id_cli), 1) from client));
